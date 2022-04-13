19ad91b36414fe833cfd330efe1718445ec0c739
zeeshan ahamd <zeeshana.codetown@gmail.com>
2022-04-13 20:13:02 +0530

    notification check

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function Counter() {
-
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('submitted')
+}
   return (
       <div>
-          <form onSubmit={alert("form submitted")}>
+          <form onSubmit={handleSubmit}>
               <input type="text" placeholder="Enter OTP" />
                 <button type="submit">Submit</button>
          </form>
