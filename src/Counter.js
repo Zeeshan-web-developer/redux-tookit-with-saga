58852c0740b7db913358ddebd0907907ac22f545
zeeshan ahamd <zeeshana.codetown@gmail.com>
2022-04-13 20:30:26 +0530

    notification check

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Counter() {
+    const [otp,setOTp]=useState('')
     const handleSubmit = (e) => {
     e.preventDefault();
-    alert('submitted')
+//    conform alert
+        var r = window.confirm("Are you sure you want to submit?");
+        if (r === true) {
+            alert("Submitted");
+        } else {
+            alert("Cancelled");
+        }
+        
 }
   return (
       <div>
           <form onSubmit={handleSubmit}>
-              <input type="text" placeholder="Enter OTP" />
+              <input type="text" placeholder="Enter OTP" value={otp} onChange={(e)=>setOTp(e.target.value)} />
                 <button type="submit">Submit</button>
          </form>
       </div>
